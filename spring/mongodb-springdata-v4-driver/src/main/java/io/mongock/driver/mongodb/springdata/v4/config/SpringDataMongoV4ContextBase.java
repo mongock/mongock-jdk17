24118c11945a7c74443accf4f23b013d485b9749
Oliver Santana <oliver.sm@gmail.com>
2024-06-10 10:17:47 +0100

    PlatformTransactionManager requirement removed. It was unnecessary because it wasn't being used (Mongock generates the transaction manager internally when needed). (#10)

--- spring/mongodb-springdata-v4-driver/src/main/java/io/mongock/driver/mongodb/springdata/v4/config/SpringDataMongoV4ContextBase.java
+++ spring/mongodb-springdata-v4-driver/src/main/java/io/mongock/driver/mongodb/springdata/v4/config/SpringDataMongoV4ContextBase.java
@@ -6,19 +6,15 @@ import io.mongock.driver.api.driver.*;
 import io.mongock.driver.mongodb.springdata.v4.*;
 import org.springframework.context.annotation.*;
 import org.springframework.data.mongodb.core.*;
-import org.springframework.transaction.*;
-
-import java.util.*;
 
 public abstract class SpringDataMongoV4ContextBase<CONFIG extends MongockConfiguration, DRIVER extends SpringDataMongoV4DriverBase> {
 
   @Bean
   public ConnectionDriver connectionDriver(MongoTemplate mongoTemplate,
                                            CONFIG config,
-                                           MongoDBConfiguration mongoDbConfig,
-                                           Optional<PlatformTransactionManager> txManagerOpt) {
-    DRIVER driver = buildDriver(mongoTemplate, config, mongoDbConfig, txManagerOpt);
-    setGenericDriverConfig(config, txManagerOpt, driver);
+                                           MongoDBConfiguration mongoDbConfig) {
+    DRIVER driver = buildDriver(mongoTemplate, config, mongoDbConfig);
+    setGenericDriverConfig(config, driver);
     setMongoDBConfig(mongoDbConfig, driver);
     driver.initialize();
     return driver;
@@ -26,13 +22,13 @@ public abstract class SpringDataMongoV4ContextBase<CONFIG extends MongockConfigu
 
   protected abstract DRIVER buildDriver(MongoTemplate mongoTemplate,
                                         CONFIG config,
-                                        MongoDBConfiguration mongoDbConfig,
-                                        Optional<PlatformTransactionManager> txManagerOpt);
+                                        MongoDBConfiguration mongoDbConfig);
 
   private void setGenericDriverConfig(CONFIG config,
-                                      Optional<PlatformTransactionManager> txManagerOpt,
                                       DRIVER driver) {
-    txManagerOpt.filter(tx -> config.getTransactionEnabled().orElse(true)).ifPresent(tx -> driver.enableTransaction());
+    if (config.getTransactionEnabled().orElse(true)) {
+      driver.enableTransaction();
+    }
     driver.setMigrationRepositoryName(config.getMigrationRepositoryName());
     driver.setLockRepositoryName(config.getLockRepositoryName());
     driver.setIndexCreation(config.isIndexCreation());
